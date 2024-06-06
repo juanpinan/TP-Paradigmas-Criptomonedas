609ca4f8f5567982622ffc4c94785503acd21a42
Santiago Garay <santi_gq@yahoo.com.ar>
2024-06-06 19:43:19 -0300

    Agregados archivos CSV.

--- src/main/java/edu/unlam/grupo5/App.java
+++ src/main/java/edu/unlam/grupo5/App.java
@@ -1,13 +1,20 @@
 package edu.unlam.grupo5;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args ) throws IOException {
+        // Enter data using BufferReader
+        System.out.println("Ingrese un dato: ");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        // Reading data using readLine
+        String name = reader.readLine();
+
+        // Printing the read line
+        System.out.println(name);
     }
 }
