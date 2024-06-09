beaf40519a15f86587d31b9f2d2c5a1b0904155b
juanpinan <jpinan@alumno.unlam.edu.ar>
2024-06-09 15:58:31 -0300

    Commit JuanP 9/6: Login y comentario
    Se agregó el login, medio peruano pero anda. Después habría que definir cómo vamos a manejar los usuarios para completar la funcionalidad que dejé comentada en el App.java

--- src/main/java/edu/unlam/grupo5/Programa.java
+++ src/main/java/edu/unlam/grupo5/Programa.java
@@ -5,6 +5,9 @@ import edu.unlam.grupo5.model.Mercado;
 import edu.unlam.grupo5.model.Usuario;
 
 import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.collections.bag.SynchronizedSortedBag;
 
 import static edu.unlam.grupo5.loader.CargadorDeDatos.cargarCriptomonedas;
 import static edu.unlam.grupo5.loader.CargadorDeDatos.cargarMercados;
@@ -26,5 +29,28 @@ public class Programa {
         System.out.println(criptomonedas);
         System.out.println(mercados);
         System.out.println(usuarios);
+    }
+    
+    public Usuario login() 
+    {
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	System.out.println("-------------------Inicio de Sesion sistema Criptodivisas-------------------\n");
+    	System.out.println("Por favor, ingrese su nombre de usuario: ");
+    	String username = scanner.nextLine();
+    	Usuario uTemp = new Usuario(username);
+    	
+    	while(!(this.usuarios.contains(uTemp)))
+    	{
+    		System.out.println("Nombre de usuario incorrecto!! Vuelva a ingresar:");
+    		username = scanner.nextLine();
+    		uTemp = new Usuario(username);
+    	} //si paso este while, tengo el usuario.
+    	
+    	int indice = usuarios.lastIndexOf(uTemp);
+    	uTemp=this.usuarios.get(indice);
+    	
+    	return uTemp;
+    	
     }
 }
--- src/main/java/edu/unlam/grupo5/model/Usuario.java
+++ src/main/java/edu/unlam/grupo5/model/Usuario.java
@@ -1,4 +1,5 @@
 package edu.unlam.grupo5.model;
+import java.math.BigDecimal;
 
 public class Usuario {
 
@@ -6,12 +7,28 @@ public class Usuario {
     private final String rolONumeroDeCuenta;
     private final String banco;
     private Double saldo; // TODO: Ver de cambiar a BigDecimal
+    private BigDecimal saldoB; // sumo este dato, yo diría de ir haciendo las cosas con double que sabemos cómo funciona
+    //y despues rompernos la cabeza para hacerlo funcionar con BigDecimal, que tiene sus propios métodos.
+    
 
     public Usuario(String nombreDeUsuario, String rolONumeroDeCuenta, String banco, Double saldo) {
         this.nombreDeUsuario = nombreDeUsuario;
         this.rolONumeroDeCuenta = rolONumeroDeCuenta;
         this.banco = banco;
         this.saldo = saldo;
+    }
+    
+    public Usuario(String nombreDeUsuario) {
+    	this.nombreDeUsuario=nombreDeUsuario;
+    	this.rolONumeroDeCuenta=null;
+    	this.banco=null;
+    }
+    
+    public Usuario ()
+    {
+    	this.nombreDeUsuario=null;
+    	this.rolONumeroDeCuenta=null;
+    	this.banco=null;
     }
 
     @Override
@@ -23,4 +40,9 @@ public class Usuario {
                 ", saldo=" + saldo +
                 "\n}";
     }
+   @Override
+   public boolean equals(Object obj) {
+	
+	return this.nombreDeUsuario.equals(((Usuario) obj).nombreDeUsuario);
+}
 }
